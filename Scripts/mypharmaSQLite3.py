79612366faca2ddaa12a224c525077c1a26c3d04
cianwalker <10021419@mydbs.ie>
2022-08-23 07:37:29 +0100

    Now back to square 2 - SQLite reading and writing properly again but still retrieving all records - also still cannot render records just entered by  GP.

--- Scripts/mypharmaSQLite3.py
+++ Scripts/mypharmaSQLite3.py
@@ -4,7 +4,7 @@ con = sqlite3.connect("mypharmaSQLite3.db")
 
 print("Database opened successfully")
 
-con.execute("create table prescriptions (priscid INTEGER PRIMARY KEY AUTOINCREMENT, PatientPPSN TEXT UNIQUE, PhysicianIMCN INTEGER,PatientName TEXT, PrescriptionContents TEXT, PrecriptionFreq INTEGER)")
+con.execute("create table prescriptions (PatientPPSN TEXT PRIMARY KEY  UNIQUE, PhysicianIMCN INTEGER, PatientName TEXT, PrescriptionContents TEXT, PrecriptionFreq INTEGER)")
 
 print("Table created successfully")
 
