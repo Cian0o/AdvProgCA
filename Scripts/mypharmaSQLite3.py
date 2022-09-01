56160651daef23d75ca7cfd888193b83d05dbd37
cianwalker <10021419@mydbs.ie>
2022-09-01 12:50:25 +0100

    Final cosmetic tweaks & testing - all functionalities and UIs rendering and executing properly - had to redo prescriptions table as removal of unique constraint did not stick from last time I worked on it.

--- Scripts/mypharmaSQLite3.py
+++ Scripts/mypharmaSQLite3.py
@@ -6,17 +6,17 @@ con = sqlite3.connect("mypharmaSQLite3.db")
 
 print("Database opened successfully")
 
-# con.execute("create table prescriptions (PatientPPSN TEXT PRIMARY KEY, PhysicianIMCN INTEGER, PatientName TEXT, PrescriptionContents TEXT, PrecriptionFreq INTEGER)")
+con.execute("create table prescriptions (PatientPPSN TEXT PRIMARY KEY, PhysicianIMCN INTEGER, PatientName TEXT, PrescriptionContents TEXT, PrecriptionFreq INTEGER)")
 #
 # con.execute("create table physicians (PhysicianIMCN INTEGER PRIMARY KEY, SurgeryName TEXT, SurgeryPhone INTEGER, SurgeryAddress TEXT, SurgeryEmail EMAIL)")
+# #
+# con.execute("create table pharmacies (PSIReg INTEGER PRIMARY KEY, PharmaName TEXT, PharmaPhone INTEGER, PharmaAddress TEXT, PharmaEmail EMAIL)")
 #
-con.execute("create table pharmacies (PSIReg INTEGER PRIMARY KEY, PharmaName TEXT, PharmaPhone INTEGER, PharmaAddress TEXT, PharmaEmail EMAIL)")
-
-con.execute("create table pharmaValidate (PSIReg INTEGER PRIMARY KEY, Trading Name TEXT, Street1 TEXT, Street2 TEXT, Street3 TEXT, Town TEXT, County TEXT, RPB Owner TEXT, Eircode TEXT, Address1 TEXT)")
-
-Validator = pd.read_excel(("/Users/cianwalker/Desktop/AdvProgCA/Extract of Pharmacies - PSI.xlsx"), skiprows=[0,1])
-
-Validator.to_sql('pharmaValidate', con, if_exists='replace',index=False)
+# con.execute("create table pharmaValidate (PSIReg INTEGER PRIMARY KEY, Trading Name TEXT, Street1 TEXT, Street2 TEXT, Street3 TEXT, Town TEXT, County TEXT, RPB Owner TEXT, Eircode TEXT, Address1 TEXT)")
+#
+# Validator = pd.read_excel(("/Users/cianwalker/Desktop/AdvProgCA/Extract of Pharmacies - PSI.xlsx"), skiprows=[0,1])
+#
+# Validator.to_sql('pharmaValidate', con, if_exists='replace',index=False)
 
 print("Table created successfully")
 
